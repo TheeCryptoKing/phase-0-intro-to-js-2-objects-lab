7ab81d16a0650bb26192cc7d702a2d09a1b83bbd
kwamebrowne <theeecryptoking@gmail.com>
2023-04-03 23:48:16 -0500

    objects lab unf

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+// Write your soluteion in this file!
+const employee = {
+    [employeeName]: "Josh Benrnard", //key:value
+    [employeeAdress]: "123 bumbleras ave",
+};
+
+employee;
+
+function updateEmployeeWithKeyAndValue(employee, employeeName) {
+    const newEmployee = {...employee};
+     return newEmployee;
+     //return newEmployee.Object.keys();
+     //return newEmployee[employeeAdress];
+}
